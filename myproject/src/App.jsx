d5d7d6f5ddca8233bb4e61c8a685d0814b4833f2
23A91A05H8 <23A91A05H8@aec.edu.in>
2025-09-10 20:30:16 +0530

    Adding files

---
+++ myproject/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import Combiningcomponents from "./Combiningcomponents";
+import FunctionComponent from "./FunctionComponent";
+
+function App() {
+  return (
+    <>
+    <FunctionComponent/>
+    </>
+    // <center>
+    // <form>
+    //   <Combiningcomponents/>
+    // </form>
+    // </center>
+
+  );
+}
+
+export default App;
---
+++ myproject/src/props.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./props.css";
+const cards  = (ListItems) => {
+    console.log(props);
+}
